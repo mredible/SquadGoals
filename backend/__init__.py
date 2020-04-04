74bfab15031df75cfe06bfff221be6718472544d
brittasrivas <47535496+brittasrivas@users.noreply.github.com>
2020-04-04 17:19:01 +0100

    Update __init__.py

--- backend/__init__.py
+++ backend/__init__.py
@@ -5,10 +5,10 @@ from flask import Flask
 
 def create_app(test_config=None):
     # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
+    app = Flask(__name__, instance_relative_config=True)   #tells the app that configuration files are relative to the instance folder located outside outside the backend package
     app.config.from_mapping(
         SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+        DATABASE=os.path.join(app.instance_path, 'backend.sqlite'),
     )
 
     if test_config is None:
